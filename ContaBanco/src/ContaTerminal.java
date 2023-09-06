f610492b1f927e374a924b2e780c4baabc553285
leonardo <leonardopsantos95@gmail.com>
2023-09-05 23:16:51 -0300

    Adicionado sistema de entrada para os dados do cliente.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numeroConta;
+        float saldo;
+        String agencia;
+        String nomeCliente;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("\tBem-vindo ao sistema Banco Terminal!\nDigite as informações necessárias para registrar seu cadastro!");
+        System.out.print("Por favor, digite o nome do senhor(a): ");
+        nomeCliente = entrada.nextLine();
+        System.out.print("O número da agência: ");
+        agencia = entrada.nextLine();
+        System.out.print("O número da conta: ");
+        numeroConta = entrada.nextInt();
+        System.out.print("O saldo da conta: ");
+        saldo = entrada.nextFloat();
+
+        System.out.println("Olá " +nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque!");
     }
 }
